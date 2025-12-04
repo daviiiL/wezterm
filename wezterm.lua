962e4b46af45eae3e7981fcd54df4a678b66c5e4
David Liu <davidas99017@gmail.com>
2025-12-03 16:19:44 -0800

    Updated config

--- wezterm.lua
+++ wezterm.lua
@@ -12,8 +12,26 @@ config.window_frame = {
 	}),
 }
 
+config.window_close_confirmation = "NeverPrompt"
+
 for k, v in pairs(window_frame) do
 	config.window_frame[k] = v
 end
+
+wezterm.on("update-right-status", function(window, pane)
+	local pname = pane:get_foreground_process_name()
+
+	if pname and pname:find("n?vim") then
+		window:set_config_overrides({
+			font = wezterm.font("SauceCodePro Nerd Font"),
+			font_size = 12,
+		})
+	else
+		window:set_config_overrides({
+			font = wezterm.font("DepartureMono Nerd Font"),
+			font_size = 12,
+		})
+	end
+end)
 
 return config
